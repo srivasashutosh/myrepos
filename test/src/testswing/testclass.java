e82f84a514be4faef5e046aa9c8b819f97060f2a
ashutosh <sriashu.05@gmail.com>
2013-10-08 12:00:26 +0530

    this is the first commit

---
+++ test/src/testswing/testclass.java
@@ -0,0 +1,37 @@
+package testswing;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class testclass {
+  public static void main(String[] args)
+  {
+	  
+	  final JFrame frame = new JFrame("JDialog Demo");    
+		final JButton btnLogin = new JButton("Click to login"); 
+		btnLogin.addActionListener(new ActionListener(){ 
+			public void actionPerformed(ActionEvent e) {               
+				LoginDialog loginDlg = new LoginDialog(frame);
+				loginDlg.setVisible(true);
+				// if logon successfully  
+				if(loginDlg.isSucceeded()){
+					btnLogin.setText("Hi Devops " );
+					}
+				}
+			});
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,300);   
+		frame.setLayout(new FlowLayout()); 
+		frame.getContentPane().add(btnLogin);
+		frame.setVisible(true);  
+		}
+	
+
+	
+  }
+
